3991942f137522166dc2a7fd356e2260e6505c47
gaston pedraza <gaston08pedraza@gmail.com>
2023-10-05 15:49:48 -0300

    add login username

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,10 +1,61 @@
+import { useState } from 'react';
 import styles from './Welcome.module.css';
+import { signInAnonymously } from "firebase/auth";
+import { auth } from "../../firebase";
 
 export default function Welcome() {
+
+  const [username, setUsername] = useState('');
+  const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleChange = e => {
+    setUsername(e.target.value);
+  }
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    if (username.trim() === "") {
+      setError('Must provide a valid username');
+    } else {
+      setIsLoading(true);
+      try {
+        localStorage.setItem('username', username);
+        await signInAnonymously(auth);
+      } catch (e) {
+        localStorage.removeItem('username', username);
+        console.log(e);
+        setError('Error while loading');
+        setIsLoading(false);
+      }
+    }
+  }
+
   return (
     <div className={styles.root}>
       <div className={styles.modal}>
-        A
+        <h1>Welcome</h1>
+        <p>Enter your username:</p>
+        <form onSubmit={handleSubmit} className={styles.form}>
+          <input 
+            type="text" 
+            onChange={handleChange}
+          />
+          <button 
+            type="submit"
+            disabled={isLoading}
+          >
+            Join
+          </button>
+          {
+            error === '' ?
+            (null)
+            :
+            (
+              <p>{error}</p>
+            )
+          }
+        </form>
       </div>
     </div>
   );
