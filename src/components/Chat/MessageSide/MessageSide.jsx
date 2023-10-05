e7cd3bbabe606e4e703b9e05e8609728a53e5a9f
gaston pedraza <gaston08pedraza@gmail.com>
2023-10-05 20:46:07 -0300

    reformat

--- src/components/Chat/MessageSide/MessageSide.jsx
+++ src/components/Chat/MessageSide/MessageSide.jsx
@@ -1,11 +1,11 @@
-import './Message.css';
+import styles from './MessageSide.module.css';
 import Information from './Information/Information';
 import Content from './Content/Content';
 import InputSide from './InputSide/InputSide';
 
 export default function Message() {
   return (
-    <div className="root">
+    <div className={styles.root}>
       <Information />
       <Content />
       <InputSide />
