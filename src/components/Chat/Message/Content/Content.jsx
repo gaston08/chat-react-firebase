05508cff5e05488859228aac50a90f1b8ceb93c1
gaston pedraza <gaston08pedraza@gmail.com>
2023-10-05 20:38:40 -0300

    read and write messages

--- src/components/Chat/Message/Content/Content.jsx
+++ src/components/Chat/Message/Content/Content.jsx
@@ -1,9 +1,47 @@
+import { useState, useEffect } from 'react';
 import styles from './Content.module.css';
+import {
+  query,
+  collection,
+  orderBy,
+  onSnapshot,
+  limit,
+} from "firebase/firestore";
+import { db } from "../../../../firebase";
 
 export default function Content() {
+
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    const q = query(
+      collection(db, "messages"),
+      orderBy("createdAt", "desc"),
+      limit(11)
+    );
+
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      let updatedData = snapshot.docs.map(doc => {
+        let data = doc.data();
+        data.id = doc.id;
+        return data;
+      });
+      setMessages(updatedData.reverse())
+    });
+    return () => unsubscribe;
+  }, []);
+
   return (
     <div className={styles.root}>
-      Content
+      {
+        messages.map(message => {
+          return (
+            <p key={message.id}>
+              {message.text}
+            </p>
+          )
+        })
+      }
     </div>
   );
 }
--- src/components/Chat/Message/InputSide/InputSide.jsx
+++ src/components/Chat/Message/InputSide/InputSide.jsx
@@ -1,16 +1,56 @@
+import { useState } from 'react';
 import styles from './InputSide.module.css';
+import { auth, db } from "../../../../firebase";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 
 export default function InputSide() {
+
+  const [message, setMessage] = useState('');
+
+  const handleChange = e => {
+    setMessage(e.target.value);
+  }
+
+  const sendMessage = async e => {
+    e.preventDefault();
+    if (message.trim() === "") {
+      return;
+    }
+
+    const { uid } = auth.currentUser;
+    const name = localStorage.getItem('username');
+
+    await addDoc(collection(db, "messages"), {
+      text: message,
+      name,
+      createdAt: serverTimestamp(),
+      uid,
+    });
+
+    setMessage('');
+    // e.scroll.current.scrollIntoView({ behavior: "smooth" });
+  }
+
   return (
     <div className={styles.root}>
-      <div className={styles.container}>
-        <div className={styles.inputBox}>
-          <input type="text" className={styles.input} />
+      <form 
+        className={styles.container}
+        onSubmit={sendMessage}
+      >
+        <div 
+          className={styles.inputBox}
+        >
+          <input 
+            type="text"
+            className={styles.input}
+            onChange={handleChange}
+            value={message}
+          />
         </div>
         <button className={styles.sendButton}>
           Send
         </button>
-      </div>
+      </form>
     </div>
   );
 }
