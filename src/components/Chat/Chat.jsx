589aa320d4853021410623a3c1fb804ba10b9c92
gaston pedraza <gaston08pedraza@gmail.com>
2023-10-06 12:32:38 -0300

    listen to url

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,12 +1,40 @@
 import styles from './Chat.module.css';
 import MessageSide from './MessageSide/MessageSide';
 import Chats from './Chats/Chats';
+import { useParams } from 'react-router-dom';
 
 export default function Chat() {
+  const params = useParams();
+  let a = window.location.search.split('=')[1];
+
   return (
     <div className={styles.root}>
       <Chats />
-      <MessageSide />
+      <>
+        {
+          a ?
+          (<MessageSide />)
+          :
+          (<NoChat />)
+        }
+      </>
+      
+    </div>
+  );
+}
+
+function NoChat() {
+  return (
+    <div
+      style={{
+        flexBasis: "50%",
+        flexGrow: "1",
+        height: "100%",
+        position: "relative",
+        display: "flex",
+      }}
+    >
+      <h1>Select a chat</h1>
     </div>
   );
 }
