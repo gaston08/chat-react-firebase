8f1546d9ea12c2148aaaf4b61e4e25c2f58d79f6
gaston pedraza <gaston08pedraza@gmail.com>
2023-10-05 22:02:10 -0300

    add utils

--- src/components/Chat/MessageSide/Content/Message/Message.jsx
+++ src/components/Chat/MessageSide/Content/Message/Message.jsx
@@ -1,4 +1,9 @@
 import styles from './Message.module.css';
+import { 
+  getInitials, 
+  generateColor,
+  invertColor,
+} from '../../../../../utils/chat';
 
 export default function Message(props) {
   const { message, isOwner } = props;
@@ -51,57 +56,4 @@ export default function Message(props) {
       }
     </div>
   );
-}
-
-function getInitials(name) {
-  let initials;
-  let arr = name.split(" ");
-  if (arr.length > 1) {
-    arr.length = 2;
-    initials = arr[0].charAt(0) + arr[1].charAt(0);
-  } else {
-    initials = name.charAt(0) + name.charAt(name.length - 1);
-  }
-  return initials;
-}
-
-function generateColor(str) {
-  let hash = 0;
-  str.split('').forEach(char => {
-    hash = char.charCodeAt(0) + ((hash << 5) - hash)
-  })
-  let colour = '#'
-  for (let i = 0; i < 3; i++) {
-    const value = (hash >> (i * 8)) & 0xff
-    colour += value.toString(16).padStart(2, '0')
-  }
-  return colour
-} 
-
-function invertColor(hex, bw) {
-  if (hex.indexOf('#') === 0) {
-      hex = hex.slice(1);
-  }
-  // convert 3-digit hex to 6-digits.
-  if (hex.length === 3) {
-      hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
-  }
-  if (hex.length !== 6) {
-      throw new Error('Invalid HEX color.');
-  }
-  var r = parseInt(hex.slice(0, 2), 16),
-      g = parseInt(hex.slice(2, 4), 16),
-      b = parseInt(hex.slice(4, 6), 16);
-  if (bw) {
-      // https://stackoverflow.com/a/3943023/112731
-      return (r * 0.299 + g * 0.587 + b * 0.114) > 186
-          ? '#000000'
-          : '#FFFFFF';
-  }
-  // invert color components
-  r = (255 - r).toString(16);
-  g = (255 - g).toString(16);
-  b = (255 - b).toString(16);
-  // pad each with zeros and return
-  return "#" + padZero(r) + padZero(g) + padZero(b);
 }
