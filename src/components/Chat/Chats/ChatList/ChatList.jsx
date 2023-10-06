e9a9827727630dd8b56596ea0692b9809a8dc6c8
gaston pedraza <gaston08pedraza@gmail.com>
2023-10-06 01:29:17 -0300

    set structure

---
+++ src/components/Chat/Chats/ChatList/ChatList.jsx
@@ -0,0 +1,9 @@
+import styles from './ChatList.module.css';
+
+export default function ChatList() {
+  return (
+    <div className={styles.root}>
+      ChatList
+    </div>
+  );
+}
--- src/components/Chat/Chats/Chats.jsx
+++ src/components/Chat/Chats/Chats.jsx
@@ -1,10 +1,14 @@
 import styles from './Chats.module.css';
 import Profile from './Profile/Profile';
+import CreateChat from './CreateChat/CreateChat';
+import ChatList from './ChatList/ChatList';
 
 export default function Chats() {
   return (
     <div className={styles.root}>
       <Profile />
+      <CreateChat />
+      <ChatList />
     </div>
   );
 }
